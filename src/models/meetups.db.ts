f1d3aa28fe76f83002dca86647dbc8773a46d553
Piotr <p.kieltyka@gmail.com>
2019-06-19 09:06:33 +0200

    polishing meetup's

--- src/models/meetups.db.ts
+++ src/models/meetups.db.ts
@@ -5,7 +5,7 @@ export const MeetupsDB: Meetup[] = [
     {
         title: 'Angular Tricity #6',
         date: 'February 19, 2019',
-        featured_image: 'https://eventil.s3.amazonaws.com/uploads/group/avatar/3672/medium_27972679_191309268288036_1032590328319884337_n.png',
+        featured_image: '../assets/angular_tricity.png',
         topics: [
             'Dynamic components & Lazy loading w Angular',
             'Introduction to Apollo GraphQL with Angular',
@@ -15,7 +15,7 @@ export const MeetupsDB: Meetup[] = [
     {
         title: 'Angular Tricity #5',
         date: 'November 6, 2018',
-        featured_image: 'https://eventil.s3.amazonaws.com/uploads/group/avatar/3672/medium_27972679_191309268288036_1032590328319884337_n.png',
+        featured_image: '../assets/angular_tricity.png',
         topics: [
             'State management with NgRx',
             'I like to move it! Animations in Angular',
@@ -25,7 +25,7 @@ export const MeetupsDB: Meetup[] = [
     {
         title: 'Angular Tricity #4',
         date: 'September 12, 2018',
-        featured_image: 'https://eventil.s3.amazonaws.com/uploads/group/avatar/3672/medium_27972679_191309268288036_1032590328319884337_n.png',
+        featured_image: '../assets/angular_tricity.png',
         topics: [
             'Angular i18n - current state and the future',
             'Domain Driven Design w Angular',
@@ -35,7 +35,7 @@ export const MeetupsDB: Meetup[] = [
     {
         title: 'Angular Tricity #3',
         date: 'June 27, 2018',
-        featured_image: 'https://eventil.s3.amazonaws.com/uploads/group/avatar/3672/medium_27972679_191309268288036_1032590328319884337_n.png',
+        featured_image: '../assets/angular_tricity.png',
         topics: [
             'Angular Schematics',
             'Angular, the bad parts',
@@ -45,7 +45,7 @@ export const MeetupsDB: Meetup[] = [
     {
         title: 'Rolling Scopes #48',
         date: 'May 25, 2018',
-        featured_image: 'https://rollingscopes.com/images/logo_rs_text.svg',
+        featured_image: '../assets/rollingscopes.svg',
         topics: [
             '\"Backend For Frontend\“ with NestJS: usage patterns',
         ]
@@ -53,7 +53,7 @@ export const MeetupsDB: Meetup[] = [
     {
         title: 'Angular Tricity #2',
         date: 'May 9, 2018',
-        featured_image: 'https://eventil.s3.amazonaws.com/uploads/group/avatar/3672/medium_27972679_191309268288036_1032590328319884337_n.png',
+        featured_image: '../assets/angular_tricity.png',
         topics: [
             'TypeScript magic types, the advanced type system',
             'Whats new in Angular 6',
@@ -63,7 +63,7 @@ export const MeetupsDB: Meetup[] = [
     {
         title: 'Angular Tricity #1',
         date: 'March 22, 2018',
-        featured_image: 'https://eventil.s3.amazonaws.com/uploads/group/avatar/3672/medium_27972679_191309268288036_1032590328319884337_n.png',
+        featured_image: '../assets/angular_tricity.png',
         topics: [
             'Component testing',
             'Directives',
@@ -73,7 +73,7 @@ export const MeetupsDB: Meetup[] = [
     {
         title: 'The Rolling Scopes #34',
         date: 'April 26, 2017',
-        featured_image: 'https://rollingscopes.com/images/logo_rs_text.svg',
+        featured_image: '../assets/rollingscopes.svg',
         topics: [
             'Accessibility Driven Development: why would you even care?',
             'Start Using Custom Elements',
