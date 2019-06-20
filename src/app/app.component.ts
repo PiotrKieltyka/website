75b6e75ed33c9827306ea5432493b46b807b5671
Piotr <p.kieltyka@gmail.com>
2019-06-20 16:28:31 +0200

    typewriting effect added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,6 @@ export class AppComponent {
   }
 
   constructor() {
-    console.log('%c%s', 'color: transparent; background: transparent; font-size: 24pt; background-clip: text; text-shadow: 0px 2px 3px rgba(243,243,243,.5);', 'Hello stranger! Are you lost?');
+    console.log('%c%s', 'color: #939393; background: transparent; font-size: 24pt; background-clip: text; text-shadow: 0px 1px 3px rgba(243,243,243,.5);', 'Hello stranger! Are you lost?');
   }
 }
