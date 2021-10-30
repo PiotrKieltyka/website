61ad54916d1ad9aa81e0f953aaf1dea447bc3661
Piotr Kieltyka <p.kieltyka@gmail.com>
2021-10-30 11:51:16 +0200

    add deploy action

--- src/app/services/websitedb.service.ts
+++ src/app/services/websitedb.service.ts
@@ -53,12 +53,12 @@ export class WebsiteDBService {
   handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
+      console.error('An error occurred:', error.message);
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
+        `Backend returned code ${error.status}, body was: `, error.message);
     }
     // Return an observable with a user-facing error message.
     return throwError(
