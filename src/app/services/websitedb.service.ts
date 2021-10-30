575a92f48fabf1c5fc4046fbe681f7acf1824ed2
PiotrKieltyka <PiotrKieltyka@users.noreply.github.com>
2021-10-30 07:20:04 +0000

    Apply formatting changes

--- src/app/services/websitedb.service.ts
+++ src/app/services/websitedb.service.ts
@@ -1,5 +1,9 @@
 import { BlogPost } from '../../models/blogpost.model';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { API_NODE_TOKEN } from './api_token';
@@ -11,33 +15,34 @@ export class WebsiteDBService {
   constructor(
     private http: HttpClient,
     @Inject(API_NODE_TOKEN) private API_NODE_TOKEN: string,
-  ) {
-  }
+  ) {}
 
   getAllPosts(): Observable<{ posts: Array<BlogPost> }> {
-    return this.http.get(this.API_NODE_TOKEN + 'posts/') as Observable<{ posts: Array<BlogPost> }>;
+    return this.http.get(this.API_NODE_TOKEN + 'posts/') as Observable<{
+      posts: Array<BlogPost>;
+    }>;
   }
 
   getPostById(id: string): Observable<BlogPost> {
-    return this.http.get(this.API_NODE_TOKEN + 'post/' + id) as Observable<BlogPost>;
+    return this.http.get(
+      this.API_NODE_TOKEN + 'post/' + id,
+    ) as Observable<BlogPost>;
   }
 
   addPost(post: BlogPost): Observable<any> {
-    return this.http
-      .post(
-        this.API_NODE_TOKEN + 'post/add',
-        JSON.stringify(post),
-        this.addHeaders(),
-      );
+    return this.http.post(
+      this.API_NODE_TOKEN + 'post/add',
+      JSON.stringify(post),
+      this.addHeaders(),
+    );
   }
 
   updatePostById(id: string, post: BlogPost): Observable<any> {
-    return this.http
-      .post(
-        this.API_NODE_TOKEN + 'post/update/' + id,
-        JSON.stringify(post),
-        this.addHeaders(),
-      );
+    return this.http.post(
+      this.API_NODE_TOKEN + 'post/update/' + id,
+      JSON.stringify(post),
+      this.addHeaders(),
+    );
   }
 
   addHeaders() {
@@ -58,10 +63,11 @@ export class WebsiteDBService {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
+        `Backend returned code ${error.status}, body was: `,
+        error.error,
+      );
     }
     // Return an observable with a user-facing error message.
-    return throwError(
-      'Something bad happened; please try again later.');
+    return throwError('Something bad happened; please try again later.');
   }
 }
